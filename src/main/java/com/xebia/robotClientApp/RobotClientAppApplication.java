e02d9682ac1f60998c60f4f22f5b884836271682
sdhawan27 <53635699+sdhawan27@users.noreply.github.com>
2020-03-01 14:25:38 +0530

    Adding consul

--- src/main/java/com/xebia/robotClientApp/RobotClientAppApplication.java
+++ src/main/java/com/xebia/robotClientApp/RobotClientAppApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
+@EnableDiscoveryClient
 public class RobotClientAppApplication extends SpringBootServletInitializer {
 
 	@Override
